760695cd3dd83ad84240c21fab48853ae5907280
jmarindgz <jmarindgz1@gmail.com>
2025-03-06 13:33:58 -0300

    Add files via upload

---
+++ hashmap/producto.java
@@ -0,0 +1,36 @@
+class Producto {
+    private String nombre;
+    private double precio;
+    private int stock;
+
+    public Producto(String nombre, double precio, int stock) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                ", stock=" + stock +
+                '}';
+    }
+}
